4f5001222954a3390c2a15da438881544e54c0a0
Jonah Siekmann <siekmanj@oregonstate.edu>
2018-02-07 03:21:56 -0800

    comments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
 import random
 import math
 from tkinter import *
 
 class Obstacle:
     def __init__(self):
+        #give an obstacle random x and y coordinates, and a random radius
         random.seed()
         
         self.radius = random.uniform(40, 50)
@@ -12,17 +12,22 @@ class Obstacle:
         self.y = random.uniform(0+self.radius, 500-self.radius)
         
     def distanceFromPoint(self, x, y):
+        #returns the distance from a pair of xy coordinates to the object
         return math.sqrt(math.pow(self.x-x, 2) + math.pow(self.y-y, 2)) - self.radius
-    
+
+#initialize an empty list
 obstacle_list = []
 
+#add 11 obstacles to the list
 for i in range(11):
     obstacle_list.append(Obstacle())
 
+#visualization stuff
 master = Tk()
 w = Canvas(master, width=1000, height=600)
 w.pack()
 
+#draw the obstacles on the canvas
 for i in obstacle_list:
     w.create_oval(i.x+i.radius, i.y+i.radius, i.x-i.radius, i.y-i.radius, fill="red")
 
