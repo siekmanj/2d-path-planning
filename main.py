f5566ca61b0919b8f0e0d484d00853ebae754f35
siekmanj <siekmanj@oregonstate.edu>
2018-02-13 14:32:38 -0800

    changed fan algorithms and moved stuff around in main

--- main.py
+++ main.py
@@ -24,21 +24,15 @@ w.pack()
 for i in obstacle_list:
     w.create_oval(i.position.x+i.radius, i.position.y+i.radius, i.position.x-i.radius, i.position.y-i.radius, fill="red")
 
+paths = [] #append your paths to this list to draw them all at once (for comparison)
+paths.append(fanpath.extendingfan(obstacle_list, waypoint_list[0], waypoint_list[1]))
+#paths.append(fanpath.simplefan(obstacle_list, waypoint_list[0], waypoint_list[1]))
+#paths.append(rubberpath.rubberpath(obstacle_list, waypoint_list))
 
-if WHICH_ALGORITHM == 0:
-    path = fanpath.simplefan(obstacle_list, waypoint_list[0], waypoint_list[1])
+for path in paths:
+	for segment in path:
+		w.create_line(segment.startPos.x,segment.startPos.y,segment.endPos.x,segment.endPos.y)
 
-    lastx = waypoint_list[0].x
-    lasty = waypoint_list[0].y
-    for coord in path:
-        w.create_line(lastx, lasty, coord.x, coord.y)
-        lastx = coord.x
-        lasty = coord.y
-else:
-    segmentspath = rubberpath.rubberpath(obstacle_list,waypoint_list)
-
-    for segment in segmentspath:
-        w.create_line(segment.startPos.x,segment.startPos.y,segment.endPos.x,segment.endPos.y)
 
 
 mainloop()
