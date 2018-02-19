f5cd99dfbedcdbcd44b9bb6a94a8d867ab572149
bamberjo <32629239+bamberjo@users.noreply.github.com>
2018-02-19 10:49:18 -0800

    Added support for multiple waypoints in all 3 algorithms

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import fieldgen
+import fanpath
 import rubberpath
 import drawObstaclesPath
 
 FIELD_WIDTH = 1000
 FIELD_HEIGHT = 600
-NUM_WAYPOINTS = 4
+NUM_WAYPOINTS = 2
 NUM_OBSTACLE = 10
+SHOW_RUBBER_STEPS = False
 USE_GUI = True#Whether to enable the gui
 
 
@@ -13,8 +15,8 @@ USE_GUI = True#Whether to enable the gui
 waypoint_list, obstacle_list = fieldgen.fieldGen(FIELD_WIDTH, FIELD_HEIGHT,USE_GUI , NUM_OBSTACLE, NUM_WAYPOINTS)
 
 paths = []  # append your paths to this list to draw them all at once (for comparison)
-#paths.append(fanpath.extendingfan(obstacle_list, waypoint_list[0], waypoint_list[1]))
-#qpaths.append(fanpath.simplefan(obstacle_list, waypoint_list[0], waypoint_list[1]))
-paths.append(rubberpath.rubberpath(obstacle_list, waypoint_list,True,FIELD_HEIGHT,FIELD_WIDTH))
+paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'simplefan'))
+paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'extendingfan'))
+paths.append(rubberpath.rubberpath(obstacle_list, waypoint_list,SHOW_RUBBER_STEPS,FIELD_HEIGHT,FIELD_WIDTH))
 
 drawObstaclesPath.drawObstaclesPath(obstacle_list,paths,FIELD_HEIGHT,FIELD_WIDTH)
