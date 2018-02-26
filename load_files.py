93cbd27dac0ad44807040e1710d2b34c2cf0d157
Reed Reese-Steiner <arleycat@gmail.com>
2018-02-26 14:18:55 -0800

    initial support for loading/saving path/waypoint/obstacle data with files working

---
+++ load_files.py
@@ -0,0 +1,15 @@
+import drawObstaclesPath
+import readFile
+
+FIELD_WIDTH = 1000
+FIELD_HEIGHT = 600
+
+PathFileName = 'paths'
+WaypointFileName = 'waypoints'
+ObstacleFileName = 'obstacles'
+
+paths = readFile.readFile(PathFileName)
+waypoint_list = readFile.readFile(WaypointFileName)
+obstacle_list = readFile.readFile(ObstacleFileName)
+
+drawObstaclesPath.drawObstaclesPath(obstacle_list,paths,FIELD_HEIGHT,FIELD_WIDTH)
---
+++ save_files.py
@@ -0,0 +1,33 @@
+import fieldgen
+import fanpath
+import rubberpath
+import writeFile
+#import json
+
+
+FIELD_WIDTH = 1000
+FIELD_HEIGHT = 600
+NUM_WAYPOINTS = 2
+NUM_OBSTACLE = 10
+SHOW_RUBBER_STEPS = False
+USE_GUI = False #Whether to enable the gui
+
+
+waypoint_list, obstacle_list = fieldgen.fieldGen(FIELD_WIDTH, FIELD_HEIGHT,USE_GUI , NUM_OBSTACLE, NUM_WAYPOINTS)
+
+paths = []  # append your paths to this list to draw them all at once (for comparison)
+paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'simplefan'))
+paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'extendingfan'))
+paths.append(rubberpath.rubberpath(obstacle_list, waypoint_list,SHOW_RUBBER_STEPS,FIELD_HEIGHT,FIELD_WIDTH))
+
+for p in range(len(paths)):
+  for i in range(len(paths[p])):
+    print('path {} segment {}: sx={}, sy={};\tex={}, ey={}'.format(p, i, paths[p][i].startPos.x, paths[p][i].startPos.y, paths[p][i].endPos.x, paths[p][i].endPos.y))
+
+PathFileName = 'paths'
+WaypointFileName = 'waypoints'
+ObstacleFileName = 'obstacles'
+
+writeFile.writeFile(PathFileName, paths)
+writeFile.writeFile(WaypointFileName, waypoint_list)
+writeFile.writeFile(ObstacleFileName, obstacle_list)
