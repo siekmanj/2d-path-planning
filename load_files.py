85b8a5b99266534f5c0230fe4a0bcacdeffe7656
Reed Reese-Steiner <arleycat@gmail.com>
2018-02-26 14:54:25 -0800

    load configuration variables from .ini file

--- load_files.py
+++ load_files.py
@@ -1,12 +1,19 @@
 import drawObstaclesPath
 import readFile
+import configparser
 
-FIELD_WIDTH = 1000
-FIELD_HEIGHT = 600
+CFG_FILE_NAME = 'config.ini'
 
-PathFileName = 'paths'
-WaypointFileName = 'waypoints'
-ObstacleFileName = 'obstacles'
+cfg = configparser.ConfigParser()
+cfg.read(CFG_FILE_NAME)
+
+FIELD_WIDTH = int(cfg['general']['FieldWidth'])
+FIELD_HEIGHT = int(cfg['general']['FieldWidth'])
+
+
+PathFileName = cfg['files']['Path']
+WaypointFileName = cfg['files']['Waypoint']
+ObstacleFileName = cfg['files']['Obstacle']
 
 paths = readFile.readFile(PathFileName)
 waypoint_list = readFile.readFile(WaypointFileName)
--- save_files.py
+++ save_files.py
@@ -2,15 +2,19 @@ import fieldgen
 import fanpath
 import rubberpath
 import writeFile
-#import json
+import configparser
 
+CFG_FILE_NAME = 'config.ini'
 
-FIELD_WIDTH = 1000
-FIELD_HEIGHT = 600
-NUM_WAYPOINTS = 2
-NUM_OBSTACLE = 10
-SHOW_RUBBER_STEPS = False
-USE_GUI = False #Whether to enable the gui
+cfg = configparser.ConfigParser()
+cfg.read(CFG_FILE_NAME)
+
+FIELD_WIDTH = int(cfg['general']['FieldWidth'])
+FIELD_HEIGHT = int(cfg['general']['FieldWidth'])
+NUM_WAYPOINTS = int(cfg['general']['NumWaypoints'])
+NUM_OBSTACLE = int(cfg['general']['NumObstacles'])
+SHOW_RUBBER_STEPS = cfg['rubberpath'].getboolean('UseRubberSteps')
+USE_GUI = cfg['fieldGen'].getboolean('UseGUI') #Whether to enable the gui
 
 
 waypoint_list, obstacle_list = fieldgen.fieldGen(FIELD_WIDTH, FIELD_HEIGHT,USE_GUI , NUM_OBSTACLE, NUM_WAYPOINTS)
@@ -24,9 +28,9 @@ for p in range(len(paths)):
   for i in range(len(paths[p])):
     print('path {} segment {}: sx={}, sy={};\tex={}, ey={}'.format(p, i, paths[p][i].startPos.x, paths[p][i].startPos.y, paths[p][i].endPos.x, paths[p][i].endPos.y))
 
-PathFileName = 'paths'
-WaypointFileName = 'waypoints'
-ObstacleFileName = 'obstacles'
+PathFileName = cfg['files']['Path']
+WaypointFileName = cfg['files']['Waypoint']
+ObstacleFileName = cfg['files']['Obstacle']
 
 writeFile.writeFile(PathFileName, paths)
 writeFile.writeFile(WaypointFileName, waypoint_list)
