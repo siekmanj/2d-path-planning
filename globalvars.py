63704634fd8cc790f10926f6bf27b1597e6b00c1
Reed Reese-Steiner <arleycat@gmail.com>
2018-02-26 17:56:34 -0800

    add config module required by last commit

---
+++ globalvars.py
@@ -0,0 +1,17 @@
+import configparser
+
+CFG_FILE_NAME = 'config.ini'
+
+cfg = configparser.ConfigParser()
+cfg.read(CFG_FILE_NAME)
+
+FIELD_WIDTH = int(cfg['general']['FieldWidth'])
+FIELD_HEIGHT = int(cfg['general']['FieldHeight'])
+NUM_WAYPOINTS = int(cfg['general']['NumWaypoints'])
+NUM_OBSTACLE = int(cfg['general']['NumObstacles'])
+SHOW_RUBBER_STEPS = cfg['rubberpath'].getboolean('UseRubberSteps')
+USE_GUI = cfg['fieldGen'].getboolean('UseGUI') #Whether to enable the gui
+
+FILENAME_PATHS = cfg['files']['Path']
+FILENAME_WAYPOINTS = cfg['files']['Waypoint']
+FILENAME_OBSTACLES = cfg['files']['Obstacle']
