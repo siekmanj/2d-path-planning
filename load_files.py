beb597c039d5b22788cf17f0acaf33ffc47637e0
Reed Reese-Steiner <arleycat@gmail.com>
2018-02-26 17:25:33 -0800

    make saving and loading into separate functions and call them from main

--- load_files.py
+++ load_files.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3
 import drawObstaclesPath
 import readFile
 import configparser
 
-CFG_FILE_NAME = 'config.ini'
+def load_files():
+  CFG_FILE_NAME = 'config.ini'
 
-cfg = configparser.ConfigParser()
-cfg.read(CFG_FILE_NAME)
+  cfg = configparser.ConfigParser()
+  cfg.read(CFG_FILE_NAME)
 
-FIELD_WIDTH = int(cfg['general']['FieldWidth'])
-FIELD_HEIGHT = int(cfg['general']['FieldHeight'])
+  FIELD_WIDTH = int(cfg['general']['FieldWidth'])
+  FIELD_HEIGHT = int(cfg['general']['FieldHeight'])
 
 
-PathFileName = cfg['files']['Path']
-WaypointFileName = cfg['files']['Waypoint']
-ObstacleFileName = cfg['files']['Obstacle']
+  PathFileName = cfg['files']['Path']
+  WaypointFileName = cfg['files']['Waypoint']
+  ObstacleFileName = cfg['files']['Obstacle']
 
-paths = readFile.readFile(PathFileName)
-waypoint_list = readFile.readFile(WaypointFileName)
-obstacle_list = readFile.readFile(ObstacleFileName)
+  paths = readFile.readFile(PathFileName)
+  waypoint_list = readFile.readFile(WaypointFileName)
+  obstacle_list = readFile.readFile(ObstacleFileName)
 
-drawObstaclesPath.drawObstaclesPath(obstacle_list,paths,FIELD_HEIGHT,FIELD_WIDTH)
+  drawObstaclesPath.drawObstaclesPath(obstacle_list,paths,FIELD_HEIGHT,FIELD_WIDTH)
--- save_files.py
+++ save_files.py
@@ -1,38 +1,38 @@
-#!/usr/bin/env python3
 import fieldgen
 import fanpath
 import rubberpath
 import writeFile
 import configparser
 
-CFG_FILE_NAME = 'config.ini'
+def save_files():
+  CFG_FILE_NAME = 'config.ini'
 
-cfg = configparser.ConfigParser()
-cfg.read(CFG_FILE_NAME)
+  cfg = configparser.ConfigParser()
+  cfg.read(CFG_FILE_NAME)
 
-FIELD_WIDTH = int(cfg['general']['FieldWidth'])
-FIELD_HEIGHT = int(cfg['general']['FieldHeight'])
-NUM_WAYPOINTS = int(cfg['general']['NumWaypoints'])
-NUM_OBSTACLE = int(cfg['general']['NumObstacles'])
-SHOW_RUBBER_STEPS = cfg['rubberpath'].getboolean('UseRubberSteps')
-USE_GUI = cfg['fieldGen'].getboolean('UseGUI') #Whether to enable the gui
+  FIELD_WIDTH = int(cfg['general']['FieldWidth'])
+  FIELD_HEIGHT = int(cfg['general']['FieldHeight'])
+  NUM_WAYPOINTS = int(cfg['general']['NumWaypoints'])
+  NUM_OBSTACLE = int(cfg['general']['NumObstacles'])
+  SHOW_RUBBER_STEPS = cfg['rubberpath'].getboolean('UseRubberSteps')
+  USE_GUI = cfg['fieldGen'].getboolean('UseGUI') #Whether to enable the gui
 
 
-waypoint_list, obstacle_list = fieldgen.fieldGen(FIELD_WIDTH, FIELD_HEIGHT,USE_GUI , NUM_OBSTACLE, NUM_WAYPOINTS)
+  waypoint_list, obstacle_list = fieldgen.fieldGen(FIELD_WIDTH, FIELD_HEIGHT,USE_GUI , NUM_OBSTACLE, NUM_WAYPOINTS)
 
-paths = []  # append your paths to this list to draw them all at once (for comparison)
-paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'simplefan', FIELD_WIDTH, FIELD_HEIGHT))
-paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'extendingfan', FIELD_WIDTH, FIELD_HEIGHT))
-paths.append(rubberpath.rubberpath(obstacle_list, waypoint_list,SHOW_RUBBER_STEPS,FIELD_HEIGHT,FIELD_WIDTH))
+  paths = []  # append your paths to this list to draw them all at once (for comparison)
+  paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'simplefan', FIELD_WIDTH, FIELD_HEIGHT))
+  paths.append(fanpath.multWaypointFan(obstacle_list,waypoint_list,'extendingfan', FIELD_WIDTH, FIELD_HEIGHT))
+  paths.append(rubberpath.rubberpath(obstacle_list, waypoint_list,SHOW_RUBBER_STEPS,FIELD_HEIGHT,FIELD_WIDTH))
 
-for p in range(len(paths)):
-  for i in range(len(paths[p])):
-    print('path {} segment {}: sx={}, sy={};\tex={}, ey={}'.format(p, i, paths[p][i].startPos.x, paths[p][i].startPos.y, paths[p][i].endPos.x, paths[p][i].endPos.y))
+  for p in range(len(paths)):
+    for i in range(len(paths[p])):
+      print('path {} segment {}: sx={}, sy={};\tex={}, ey={}'.format(p, i, paths[p][i].startPos.x, paths[p][i].startPos.y, paths[p][i].endPos.x, paths[p][i].endPos.y))
 
-PathFileName = cfg['files']['Path']
-WaypointFileName = cfg['files']['Waypoint']
-ObstacleFileName = cfg['files']['Obstacle']
+  PathFileName = cfg['files']['Path']
+  WaypointFileName = cfg['files']['Waypoint']
+  ObstacleFileName = cfg['files']['Obstacle']
 
-writeFile.writeFile(PathFileName, paths)
-writeFile.writeFile(WaypointFileName, waypoint_list)
-writeFile.writeFile(ObstacleFileName, obstacle_list)
+  writeFile.writeFile(PathFileName, paths)
+  writeFile.writeFile(WaypointFileName, waypoint_list)
+  writeFile.writeFile(ObstacleFileName, obstacle_list)
