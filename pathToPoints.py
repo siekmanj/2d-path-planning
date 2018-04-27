c59dff9b68926864041ce7e8fcf9c5858130b372
bamberjo <32629239+bamberjo@users.noreply.github.com>
2018-04-26 18:11:23 -0700

    Added a little bit more integration for the motion profiling code. Known Issues: -Spline fit may need to more closely match the points -No points given for when the path goes around an arc segment. -Still need to get the path to points to work -Need to add some matplotlib visualizations of the spline

--- pathToPoints.py
+++ pathToPoints.py
@@ -6,9 +6,10 @@ import numpy as np
 def pathToPoints(path):
     pathWaypoints = np.ones((2,len(path)))
     #Set the first point to the start of the first segment
+    for i in range(len(path)):
+        pathWaypoints[0, i] = path[i].startPos.x
+        pathWaypoints[1, i] = path[i].startPos.y
 
-    pathWaypoints[0, 0:-1] = path[0:-1].startPos.x
-    pathWaypoints[1, 0:-1] = path[0:-1].startPos.y
     pathWaypoints[0, -1] = path[-1].endPos.x
     pathWaypoints[1, -1] = path[-1].endPos.y
 
