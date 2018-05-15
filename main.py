9aa3d6edd0861f9369c59ce7f0771ce63317d031
bamberjo <32629239+bamberjo@users.noreply.github.com>
2018-05-14 23:34:07 -0700

    Merge branch 'master' of C:\Users\jwbam\Documents\Aerial\Autonomous Motion\2d-path-planning with conflicts.

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import load_files
 import save_files
 import config
 import fieldgen
-
 import tanDjikstra
-
+import planPath
+import numpy as np
 
 from drawObstaclesPath import drawObstaclesPath
 
@@ -16,6 +16,15 @@ waypoint_list, obstacle_list, bound_segments = fieldgen.fieldGen(config.FIELD_WI
                                                  config.NUM_WAYPOINTS,
                                                  config.NUM_BOUND_PTS)
 
+#*****I don't know how to put this in the config file, but should figure it out. Temporarily defined here
+fc = {"thrust":100,
+      "mass":4,
+      "density":1.225,
+      "cd":1,
+      "refarea":.25,
+      "veff": 14}#Flight characteristics of the drone
+
+# STATIC OBSTACLE PRINTING
 paths = []  # append your paths to this list to draw them all at once (for comparison)
 realpath, allpaths, nonintersectingpath = tanDjikstra.tangentDjikstra(waypoint_list,obstacle_list,bound_segments)
 #paths.append(allpaths)
@@ -29,3 +38,11 @@ paths.append(realpath)
 save_files.save_files(paths, waypoint_list, obstacle_list)
 # load_files.load_files()
 drawObstaclesPath(obstacle_list, paths, bound_segments, waypoint_list, config.FIELD_HEIGHT, config.FIELD_WIDTH)
+
+# ANIMATION/MOVING OBSTACLE PRINTING
+
+posTimes = planPath.planPath('tandji',obstacle_list,waypoint_list[1:],bound_segments,waypoint_list[0],fc,config)
+
+#Save the positions and the times to a csv file
+np.savetxt('AerialPath.csv',posTimes,delimiter=',')
+
